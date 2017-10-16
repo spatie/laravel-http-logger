5f4a5f7848889c4849e9db89b4e1b060f00c1804
freek <freek@spatie.be>
2017-10-16 19:39:59 +0200

    convert to comment blocks

--- config/http-logger.php
+++ config/http-logger.php
@@ -2,13 +2,13 @@
 
 return [
 
-    /**
+    /*
      * The log profile used to log requests. A log profile implements the `LogProfile` class,
      * determines whether a request will be logged or not, and how the message is formatted.
      */
     'log_profile' => \Spatie\HttpLogger\DefaultLogProfile::class,
 
-    /**
+    /*
      * Filter out body fields which will never be logged.
      */
     'except' => [
