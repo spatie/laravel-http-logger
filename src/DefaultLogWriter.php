a94651b4880b478d05f1ad7ddb47fed42e4fa7a0
Tobias van Beek <t.vanbeek@tjvb.nl>
2019-11-14 12:37:18 +0100

    Add the support for arrays with files in a request

--- src/DefaultLogWriter.php
+++ src/DefaultLogWriter.php
@@ -3,6 +3,7 @@
 namespace Spatie\HttpLogger;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
@@ -16,12 +17,24 @@ class DefaultLogWriter implements LogWriter
 
         $bodyAsJson = json_encode($request->except(config('http-logger.except')));
 
-        $files = array_map(function (UploadedFile $file) {
-            return $file->getClientOriginalName();
-        }, iterator_to_array($request->files));
-
-        $message = "{$method} {$uri} - Body: {$bodyAsJson} - Files: ".implode(', ', $files);
+        $files = (new Collection(iterator_to_array($request->files)))
+            ->map([$this, 'flatFiles'])
+            ->flatten()
+            ->implode(',')
+        ;
+        $message = "{$method} {$uri} - Body: {$bodyAsJson} - Files: ". $files;
 
         Log::info($message);
+    }
+
+    public function flatFiles($file)
+    {
+        if ($file instanceof UploadedFile) {
+            return $file->getClientOriginalName();
+        }
+        if (is_array($file)) {
+            return array_map([$this, 'flatFiles'], $file);
+        }
+        return (string) $file;
     }
 }
